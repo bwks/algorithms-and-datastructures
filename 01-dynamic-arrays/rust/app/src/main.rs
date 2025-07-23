5ecb121119b4e8c957bc377a95e331b249e867b1
Brad Searle <bradleysearle@gmail.com>
2025-07-23 08:48:29 +0000

    dynamic arrays

---
+++ 01-dynamic-arrays/rust/app/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Create a dynamic array with a vector
+    let dynamic_array: Vec<i32> = vec![1, 2, 3, 4, 5];
+
+    for element in dynamic_array {
+        println!("Hello: {}", element)
+    }
+}
